2d26855af20a263900836d6efd078e51a0f12c70
davips <dpsabc@gmail.com>
2018-09-05 19:15:33 +0100

    outs select kernel at start and for error 3000itrs 4x4

--- new_ocean.py
+++ new_ocean.py
@@ -6,9 +6,12 @@ from random import randint
 from plotter import Plotter
 
 # Induce model.
-side, budget, na, nb, f = 4, 100, 100000000, 1000, f5
+print("out: Início")
+side, budget, na, nb, f = 4, 100, 100000000, 3000, f5
 (first_xys, first_zs), (TSxy, TSz) = train_data(side, f), test_data(f)
-kernel = Matern(length_scale_bounds=(0.000001, 100000), nu=1.6) #TODO kernel_selector(first_xys, first_zs)
+print("out: Select initial kernel.")
+# kernel = Matern(length_scale_bounds=(0.000001, 100000), nu=1.6)
+kernel = kernel_selector(first_xys, first_zs)
 print(type(kernel))
 model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=25, copy_X_train=True, random_state=42)
 model.fit(first_xys, first_zs)
@@ -17,6 +20,7 @@ trip_xys, trip_zs, trip_var_min, a, depot, plot = [], [], 9999999, 0, (-0.000000
 if plot: plotter = Plotter('surface')
 
 # Add maximum amount of feasible points for the given budget.
+print('out: Adding random...')
 while True:
     trip_xys.append((uniform(), uniform()))
     tour, feasible, cost = plan_tour([depot] + trip_xys, budget, exact=True)
@@ -29,6 +33,7 @@ while True:
 
 for a in range(0, na):
     # Add points between neighboring cities.
+    print('out: Adding neighbors...')
     old_tour = tour.copy()
     old_trip_xys = trip_xys.copy()
     while True:
@@ -43,6 +48,7 @@ for a in range(0, na):
         old_tour = tour.copy()
 
     # Distort one city at a time.
+    print('out: Distorting...')
     trip_var_max = evalu_var(model, trip_xys)
     new_trip_xys = trip_xys.copy()
     for b in range(0, nb):
@@ -58,6 +64,7 @@ for a in range(0, na):
     trip_xys = new_trip_xys.copy()
 
     # Induce model with simulated points.
+    print("out: Inducing with simulated...")
     trip_zs = model.predict(trip_xys, return_std=False)
     # kernel = kernel_selector(first_xys + trip_xys, first_zs + list(trip_zs))
     model2 = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=25, copy_X_train=True)
@@ -72,7 +79,8 @@ for a in range(0, na):
         trip_xys = new_trip_xys2.copy()
 
     # Logging.
-    # kernel = kernel_selector(first_xys + trip_xys, first_zs + probe(f, trip_xys)) #TODO descomentar na versão final
+    print("out: Inducing with real to evaluate error...")
+    kernel = kernel_selector(first_xys + trip_xys, first_zs + probe(f, trip_xys)) #TODO descomentar na versão final
     model3 = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=25, copy_X_train=True, random_state=42)
     model3.fit(first_xys + trip_xys, first_zs + probe(f, trip_xys))
     error = evalu_sum(model3, TSxy, TSz)
