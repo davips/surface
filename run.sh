c6c77208c0a3d63f2af5ab35e6bf13eabe468e49
davips <dpsabc@gmail.com>
2018-11-19 17:32:03 +0000

    swarm in sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ export mode="$1"      # off (static) or on (dynamic) - line version
 
 for function in `seq 1 10`; do
   for gridsize in `echo 10 7 4`; do
-    for algorithm in `echo 1c`; do    # sw = Particle Swarm Optmization (PSO parameters should be choosen inside swarm.py; currently 4000 iterations and pop. size 100)
+    for algorithm in `echo 1c sw`; do    # sw = Particle Swarm Optmization (PSO parameters should be choosen inside swarm.py; currently 4000 iterations and pop. size 100)
       echo f$function-$seed-${gridsize}x$gridsize-$budget-$algorithm-$timelimit.$mode.log ...
       time python -u ocean.py dontplot $seed $timelimit $logafter $gridsize $budget $function $algorithm $mode 2> >(grep --line-buffered -v gaussian_process | grep --line-buffered -v np.newaxis | grep --line-buffered -v convergence_dict | grep --line-buffered -v warnings.warn) | grep --line-buffered res: | tee f$function-$seed-${gridsize}x$gridsize-$budget-$algorithm-$timelimit.$mode.log
     done
