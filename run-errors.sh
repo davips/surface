d7f0d47274adaafa8b4ab532e4453b5a14c61244
davips <dpsabc@gmail.com>
2018-11-23 17:20:17 +0000

    py3

--- run-errors.sh
+++ run-errors.sh
@@ -9,7 +9,7 @@ for algorithm in `echo 1c sw`; do    # sw = Particle Swarm Optmization (PSO para
   for function in `seq 5 10; seq 1 4`; do
     for gridsize in `echo 7 4 10`; do
       echo f$function-$seed-${gridsize}x$gridsize-$budget-$algorithm-$timelimit.$mode.log ...
-      time python -u ocean.py dontplot $seed $timelimit $logafter $gridsize $budget $function $algorithm $mode 2> >(grep --line-buffered -v gaussian_process | grep --line-buffered -v np.newaxis | grep --line-buffered -v convergence_dict | grep --line-buffered -v warnings.warn)
+      time python3 -u ocean.py dontplot $seed $timelimit $logafter $gridsize $budget $function $algorithm $mode 2> >(grep --line-buffered -v gaussian_process | grep --line-buffered -v np.newaxis | grep --line-buffered -v convergence_dict | grep --line-buffered -v warnings.warn)
     done
   done
 done
